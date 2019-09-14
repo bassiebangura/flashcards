cd22028397e7ebe207aeeb82300d168fed73482e
bassiebangura <devbassiebangura@gmail.com>
2019-09-14 12:15:35 -0400

    Install pug and create pug file.

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+app.set('view engine', 'pug')
+
 app.listen(PORT, console.log("server is up and running"));
 app.get("/", (req, res, next) => {
-  res.send("Hello Boy!!")
-} )
+  res.send("<h1>Hello boy</h1>");
+});
+
+app.get("/hello", (req, res, next) => {
+  res.send("<h1>Hello js boy</h1>");
+});
