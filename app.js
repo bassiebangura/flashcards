ccad1d8c0e1a2b3d9f7d6e4453f18b94a24d1744
bassiebangura <devbassiebangura@gmail.com>
2019-09-14 15:53:49 -0400

    Add a cookie when form is posted

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 const PORT = 3000;
-const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'purple'
-  ];
+const colors = ["red", "orange", "yellow", "green", "blue", "purple"];
 
 app.set("view engine", "pug");
 
@@ -17,10 +14,19 @@ app.get("/", (req, res, next) => {
   res.render("index");
 });
 
-// app.get("/table", (req, res, next) => {
-//     res.render("table", {firstName: "Shabreya", lastName: "Bangura"});
-//   });
+app.get("/hello", (req, res, next) => {
+  res.render("hello", { name: req.cookies.username });
+});
+
+app.post("/hello", (req, res, next) => {
+  res.cookie("username", req.body.username);
+  res.render("hello", { name: req.body.username });
+});
 
 app.get("/cards", (req, res, next) => {
-  res.render("card", { prompt: "Who is buried in Grant's tomb.", hint:"Think whose tomb it might be", colors  });
+  res.render("card", {
+    prompt: "Who is buried in Grant's tomb.",
+    hint: "Think whose tomb it might be",
+    colors
+  });
 });
