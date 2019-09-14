afd5efd5247286bfe47da9e7da078ab8bcffb054
bassiebangura <devbassiebangura@gmail.com>
2019-09-14 11:56:33 -0400

    Add nodemon.

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 app.listen(PORT, console.log("server is up and running"));
+app.get("/", (req, res, next) => {
+  res.send("Hello Boy!!")
+} )
