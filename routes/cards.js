3e6a8639d4a346872afe7cb978611be217bf937b
bassiebangura <devbassiebangura@gmail.com>
2019-09-14 22:43:30 -0400

    Add data using json file.

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const { data } = require("../data/flashcarddata.json");
 
 //handles request to path: /cards
-router.get('/', (req, res, next) => {
-    res.render("card", {
-      prompt: "Who is buried in Grant's tomb."
-    });
+router.get("/", (req, res, next) => {
+  res.render("card", {
+    prompt: "Who is buried in Grant's tomb."
   });
+});
 
-  module.exports = router;
+module.exports = router;
