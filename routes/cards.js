48ea81c1bc4fa61292c78ba81a614024c6a94169
bassiebangura <devbassiebangura@gmail.com>
2019-09-16 21:14:41 -0400

    Create question and answer link.

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { data } = require("../data/flashcarddata.json");
-
+const { data } = require("../data/flashcardData.json");
+const { cards } = data;
 //handles request to path: /cards
-router.get("/", (req, res, next) => {
-  res.render("card", {
-    prompt: "Who is buried in Grant's tomb."
-  });
+router.get("/:id", (req, res, next) => {
+  const { side } = req.query;
+  const { id } = req.params;
+  const text = cards[id][side];
+  const { hint } = cards[id];
+  const templateData = { text };
+  if (side === "question") {
+    templateData.hint = hint;
+    templateData.toggleAnswerAndQuestion = "Answer";
+  } else {
+    templateData.toggleAnswerAndQuestion = "Question";
+  }
+  res.render("card", templateData);
 });
 
 module.exports = router;
