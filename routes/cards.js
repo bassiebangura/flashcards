b837a712ed5610ab586e9a5c05780dea3047ce08
bassiebangura <devbassiebangura@gmail.com>
2019-09-18 13:10:43 -0400

    Add public folder to server static files.

--- routes/cards.js
+++ routes/cards.js
@@ -5,12 +5,13 @@ const { cards } = data;
 //handles request to path: /cards
 router.get("/:id", (req, res, next) => {
   const { side } = req.query;
+  let name = req.cookies.username
   const { id } = req.params;
   const text = cards[id][side];
   const { hint } = cards[id];
-  const templateData = { id, text };
+  const templateData = { id, text, name };
   if (!side) {
-    res.redirect(`/cards/${id}?side=question`);
+    return res.redirect(`/cards/${id}?side=question`);
   }
   //determines the side to show base on the value of the 'side' key
   if (side === "question") {
